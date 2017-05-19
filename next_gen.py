d3a670a92a40c5e2565e0ba6aa6a70df7993b361
Joe Server <joe@jserver.io>
2017-05-18 19:47:10 -0700

    moving forward

--- next_gen.py
+++ next_gen.py
@@ -1,12 +1,13 @@
 import shutil
+import socket
 import time
-from socket import * 
 
 import face_recognition
 import requests
 from PIL import Image
 
-ctrl_cmd = ['forward', 'backward', 'left', 'right', 'stop', 'read cpu_temp', 'home', 'distance', 'x+', 'x-', 'y+', 'y-', 'xy_home']
+
+ctrl_cmd = ['forward', 'backward', 'left', 'right', 'stop', 'read cpu_temp', 'home', 'distance', 'x+', 'x-', 'y+', 'y-', 'xy_home']  # noqa
 
 # top = Tk()   # Create a top window
 # top.title('Sunfounder Raspberry Pi Smart Video Car')
@@ -16,15 +17,68 @@ PORT = 21567
 BUFSIZ = 1024             # buffer size
 ADDR = (HOST, PORT)
 
-# tcpCliSock = socket(AF_INET, SOCK_STREAM)   # Create a socket
-# tcpCliSock.connect(ADDR)                    # Connect with the server
+tcpCliSock = socket(socket.AF_INET, socket.SOCK_STREAM)   # Create a socket
+tcpCliSock.connect(ADDR)                    # Connect with the server
 
+
+def forward_fun(event):
+    print 'forward'
+    tcpCliSock.send('forward')
+
+
+def backward_fun(event):
+    print 'backward'
+    tcpCliSock.send('backward')
+
+
+def left_fun(event):
+    print 'left'
+    tcpCliSock.send('left')
+
+
+def right_fun(event):
+    print 'right'
+    tcpCliSock.send('right')
+
+
+def stop_fun(event):
+    print 'stop'
+    tcpCliSock.send('stop')
+
+
+def home_fun(event):
+    print 'home'
+    tcpCliSock.send('home')
+
+
+def x_increase(event):
+    print 'x+'
+    tcpCliSock.send('x+')
+
+
+def x_decrease(event):
+    print 'x-'
+    tcpCliSock.send('x-')
+
+
+def y_increase(event):
+    print 'y+'
+    tcpCliSock.send('y+')
+
+
+def y_decrease(event):
+    print 'y-'
+    tcpCliSock.send('y-')
+
+
+def xy_home(event):
+    print 'xy_home'
+    tcpCliSock.send('xy_home')
 
 
 def face_detect():
     unknown_image = face_recognition.load_image_file('temp.jpg')
     known_image = face_recognition.load_image_file('known.jpg')
-    state = None
     try:
         scott_encoding = face_recognition.face_encodings(known_image)[0]
         unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
@@ -49,21 +103,29 @@ def grab_image():
     image = image.rotate(180)
     image.save('temp.jpg')
 
+
 if __name__ == '__main__':
     requests.get('http://192.168.1.217:8000/led/setup')
+    xy_home()
     while True:
         grab_image()
         match = face_detect()
         if match == 'green':
             requests.get('http://192.168.1.217:8000/led/set_green')
+            break
 
         elif match == 'red':
             requests.get('http://192.168.1.217:8000/led/set_red')
-        
+
         elif match == 'blue':
             requests.get('http://192.168.1.217:8000/led/set_blue')
 
         else:
-            import pdb; pdb.set_trace()
-        time.sleep(2)
+            print 'invalid state'
+
+        forward_fun()
+        time.sleep(1)
+        stop_fun()
+
+    stop_fun()
     requests.get('http://192.168.1.217:8000/led/destroy')

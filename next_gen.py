ca40eaf77c1b930e0ea26130ba980cda5531aeda
SRserves85 <scottrothbarth@MacBook-Pro-2.local>
2017-05-18 19:09:25 -0700

    blue and gree!!!

--- next_gen.py
+++ next_gen.py
@@ -24,18 +24,19 @@ ADDR = (HOST, PORT)
 def face_detect():
     unknown_image = face_recognition.load_image_file('temp.jpg')
     known_image = face_recognition.load_image_file('known.jpg')
-
+    state = None
     try:
         scott_encoding = face_recognition.face_encodings(known_image)[0]
         unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
     except:
-        print "NO FACES!!"
-        return False
+        print 'No Faces'
+        return 'blue'
 
     for x in face_recognition.compare_faces([scott_encoding], unknown_encoding):
         if x:
-            return True
-            print "BOOM BABY!!"
+            return 'green'
+
+    return 'red'
 
 
 def grab_image():
@@ -49,14 +50,20 @@ def grab_image():
     image.save('temp.jpg')
 
 if __name__ == '__main__':
-    requests.get('http://192.168.1.217:8000/led/set_off')
+    requests.get('http://192.168.1.217:8000/led/setup')
     while True:
         grab_image()
         match = face_detect()
-        if match:
-            print 'MATCHED'
+        if match == 'green':
             requests.get('http://192.168.1.217:8000/led/set_green')
-        else:
-            print "NOT MATCHED"
+
+        elif match == 'red':
             requests.get('http://192.168.1.217:8000/led/set_red')
+        
+        elif match == 'blue':
+            requests.get('http://192.168.1.217:8000/led/set_blue')
+
+        else:
+            import pdb; pdb.set_trace()
         time.sleep(2)
+    requests.get('http://192.168.1.217:8000/led/destroy')
